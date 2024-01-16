a435eecb09cea9a65574b5e45b0c4378451482a4
tjwjddls930 <jungin09307@gmail.com>
2024-01-16 21:20:11 +0900

    head: metadata added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '@/styles/globals.css';
 import { LanguageProvider } from '@/contexts/Langaugecontext';
 import { FontsizeProvider } from '@/contexts/Fontsizecontext';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <LanguageProvider>
-    <FontsizeProvider>
-      <Component {...pageProps} />
-    </FontsizeProvider>
-  </LanguageProvider>
+  return <>
+    <Head>
+      <title>쉐마미술관 AI키오스크 모바일</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="쉐마미술관 AI 키오스크 by Metalab"
+      />
+      {/* <meta property="og:image" content="/og-image.png" /> */}
+    </Head>
+    <LanguageProvider>
+      <FontsizeProvider>
+        <Component {...pageProps} />
+      </FontsizeProvider>
+    </LanguageProvider>
+  </>
 }
